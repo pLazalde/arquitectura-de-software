8f8b4927de91f53ae9e85715f8091ba7e066dec2
pLazalde <145162726+pLazalde@users.noreply.github.com>
2026-04-14 16:52:22 -0700

    Update HomeTheaterTestDrive.java

--- Patron Fachada/HomeTheaterTestDrive.java
+++ Patron Fachada/HomeTheaterTestDrive.java
@@ -1,6 +1,5 @@
 public class HomeTheaterTestDrive {
     public static void main(String[] args) {
-        // 1. Instanciar los componentes del subsistema
         Amplifier amp = new Amplifier();
         Tuner tuner = new Tuner();
         DvdPlayer dvd = new DvdPlayer();
@@ -10,12 +9,10 @@ public class HomeTheaterTestDrive {
         TheaterLights lights = new TheaterLights();
         PopcornPopper popper = new PopcornPopper();
 
-        // 2. Instanciar la Fachada pasándole todos los componentes
         HomeTheaterFacade homeTheater = new HomeTheaterFacade(
             amp, tuner, dvd, cd, projector, screen, lights, popper
         );
 
-        // 3. Usar la interfaz simplificada
         homeTheater.watchMovie("Raiders of the Lost Ark");
         homeTheater.endMovie();
     }
