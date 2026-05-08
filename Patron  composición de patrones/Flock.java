b7f9eb98b897e9f5df27f785f9b646be40525b3d
pLazalde <145162726+pLazalde@users.noreply.github.com>
2026-05-07 21:22:02 -0700

    Composicion

---
+++ Patron  composición de patrones/Flock.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Flock implements Quackable {
+    ArrayList<Quackable> quackers = new ArrayList<>();
+
+    public void add(Quackable quacker) {
+        quackers.add(quacker);
+    }
+
+    @Override
+    public void quack() {
+        for (Quackable quacker : quackers) {
+            quacker.quack();
+        }
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        Iterator<Quackable> iterator = quackers.iterator();
+        while (iterator.hasNext()) {
+            Quackable quacker = iterator.next();
+            quacker.registerObserver(observer);
+        }
+    }
+
+    @Override
+    public void notifyObservers() {
+    }
+}
